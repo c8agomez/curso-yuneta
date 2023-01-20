51327fd0d73cc9ee103b751c132e77064807fb4d
yuneta <cochoa@mulesol.es>
2023-01-20 17:34:21 +0100

    Primera subida python, pdte revision

---
+++ 02-print-hello-world-loop/python/src/hello_world_loop_02.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+#----------------------------------------------------------------#
+#  Hello world loop 02
+#----------------------------------------------------------------#
+version = "1.0"
+
+#---------------------------------------#
+#       Code
+#---------------------------------------#
+import time
+
+#-----------------------------------------------#
+# Mide tiempo
+#-----------------------------------------------#
+def mide_tiempo(funcion):
+    def funcion_medida(*args, **kwargs):
+        inicio = time.time()
+        c = funcion(*args, **kwargs)
+        elapsed_time=time.time()-inicio
+        print("Spent time: %.10f s. to print" % elapsed_time)
+        return c
+    return funcion_medida
+
+#-----------------------------------------------#
+# Hello loop con parámetro
+#-----------------------------------------------#
+@mide_tiempo
+def hello_loop_t(repeat):
+    repeat=int(repeat)
+    for x in range(1,repeat+1):
+        print(x ,'Hello World')
+
+#-----------------------------------------------#
+# Hello loop default
+#-----------------------------------------------#
+@mide_tiempo
+def hello_loop_d():
+    num=100
+    for x in range(1,num+1):
+        print(x ,'Hello World')
+
+
+
+#-----------------------------------------------#
+#   Main
+#-----------------------------------------------#
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Hello world loop"
+    )
+    parser.version = 'Hello_word02_1.0.0'
+
+    parser.add_argument(
+        '-r',
+        '--repeat',
+        help="N times repeat"
+    )
+
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        help="Version del programa"
+    )
+
+    args = parser.parse_args()
+
+    if not args.repeat:
+        hello_loop_d()
+
+    else:
+        hello_loop_t(
+            repeat=args.repeat,
+        )
