1b2cfa20f72b851e6f9902d5d225806d3fdf1cb4
yuneta <cochoa@mulesol.es>
2023-01-23 16:50:42 +0100

    Fix error

--- 02-print-hello-world-loop/javascript/src/hello_world_loop_02.js
+++ 02-print-hello-world-loop/javascript/src/hello_world_loop_02.js
@@ -3,41 +3,44 @@
 const { Command } = require('commander');
 const program = new Command();
 
+
+
 function main() {
-  var repeat = 100;
+    var repeat = 100;
 
-// Opciones
+    // Opciones
 
-  program
-    .name('string-util')
-    .description('Hello world loop')
-    .version('1.0.0')
-    .usage('[-h] [-v] [-r REPEAT]')
-    .option('-r, --repeat <REPEAT>', 'N times repeat "Hello world". (Default=100)', parseInt)
-    .parse(process.argv);
+    program
+        .name('string-util')
+        .description('Hello world loop')
+        .version('1.0.0')
+        .usage('[-h] [-v] [-r REPEAT]')
+        .option('-r, --repeat <REPEAT>', 'N times repeat "Hello world". (Default=100)', parseInt)
+        .parse(process.argv);
 
-  const options = program.opts();
+    const options = program.opts();
 
-  if (options.repeat) {
-      repeat=options.repeat
-  }
+    if (options.repeat) {
+        repeat=options.repeat
+    }
 
-//Inicio tiempo
-//   const start = process.hrtime(); TODO pendiente revisar fórmula
-  const NS_PER_SEC = 1e9;
-  const MS_PER_NS = 1e-6
-  const time = process.hrtime();
-// bucle
-  for (let i=1;i<=repeat; i++) {
-      console.log('Hello World',i);
-  }
-//Fin tiempo
-//   const end = process.hrtime(start); TODO pendiente revisar fórmula
+    //Inicio tiempo
+    const time = process.hrtime();
+    const NS_PER_SEC = 1e9;
+    const MS_PER_NS = 1e-6
+    //const time = process.hrtime(); TODO otro método
+    // bucle
+    for (let i=1;i<=repeat; i++) {
+        console.log('Hello World',i);
+    }
 
-//Impresión tiempo tardado
-//   console.log(`Spent time ${(end[0] +(end[1]/1000000)).toFixed(3)}s. to print ${repeat} times.`) TODO pendiente revisar fórmula
-  const diff = process.hrtime(time);
-  console.log(`Spent time ${ (((diff[0] * NS_PER_SEC + diff[1])  * MS_PER_NS)/1000).toFixed(3) } s. to print ${repeat} times.`)
+    //Fin tiempo
+    //   const end = process.hrtime(start); TODO otro método
+
+    //Impresión tiempo tardado
+    //   console.log(`Spent time ${((end[0])+(end[1])/1000000000).toFixed(3)}s. to print ${repeat} times.`) TODO otro método
+    const diff = process.hrtime(time);
+    console.log(`Spent time ${ (((diff[0] * NS_PER_SEC + diff[1])  * MS_PER_NS)/1000).toFixed(3) } s. to print ${repeat} times.`)
 }
 
 main ();
